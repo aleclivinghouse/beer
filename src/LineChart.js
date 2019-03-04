0eb619f12961206641878c88cb77207f3bde50c9
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-03-03 17:29:14 -0800

    colors and hover over

--- src/LineChart.js
+++ src/LineChart.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
 import {select, csv, scaleLinear, max, min, scaleBand, axisLeft, axisBottom} from 'd3';
+import './LineChart.css';
 
 class LineChart extends Component {
    componentWillMount() {
@@ -42,7 +43,7 @@ class LineChart extends Component {
 
       const yAxis = axisLeft(yScale);
       const xAxis = axisBottom(xScale)
-
+      var div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
       g.selectAll('circle').data(data)
           .enter().append('circle')
           .attr('cy', d=> yScale(d.ibu))
@@ -50,6 +51,14 @@ class LineChart extends Component {
           .attr("r", 5)
           .attr("fill", (d) => {
             return categoryFill(d.category)
+          })
+          .on("mouseover", (d) => {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html((d.name) + "<br/>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
           })
           yAxis(g.append('g'));
           xAxis(g.append('g').attr('transform', `translate(0, ${innerHeight})`))
